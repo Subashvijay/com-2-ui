5a5633dfe22ccdb0ca1929b43615e030b0807e72
Subash <46962601+Subashvijay@users.noreply.github.com>
2021-06-21 12:22:15 +0530

    adding commponents

--- tweet-app/src/app/app.module.ts
+++ tweet-app/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { TweetComponent } from './components/tweet/tweet.component';
+import { PeoplesComponent } from './components/peoples/peoples.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    HomePageComponent,
+    TweetComponent,
+    PeoplesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
